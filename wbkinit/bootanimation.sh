db4ab086ad9a22bb5e8df0ee02da3800d723e8ea
sukso96100 <sukso96100@gmail.com>
2012-07-26 10:44:03 +0900

    Added android standard bootanimation support | Moved :userinit.sh, bootanimation, bootanimation.sh

--- wbkinit/bootanimation.sh
+++ wbkinit/bootanimation.sh
@@ -1,9 +1,11 @@
 #!/sbin/ext/busybox sh
 
+#Standard bootanimation support
+
 if [ -f /data/local/bootanimation.bin ]; then
   /data/local/bootanimation.bin
 elif [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ]; then
-  /sbin/bootanimation
+  /wbkinit/bootanimation
 else
   /system/bin/samsungani
 fi;
